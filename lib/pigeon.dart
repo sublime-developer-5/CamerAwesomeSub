5eb9c959d213094fbf6298b41d4d51763f9eb52e
sublime-developer-5 <developer5@sublimetechnocorp.com>
2025-08-12 18:13:36 +0530

    some fixes

--- lib/pigeon.dart
+++ lib/pigeon.dart
@@ -126,6 +126,14 @@ class PreviewSize {
       height: result[1]! as double,
     );
   }
+
+  Size toSize() => Size(width, height);
+
+  /// Returns a new [PreviewSize] with [width] and [height] inverted.
+  /// Useful when the preview size is given in portrait mode but the camera
+  /// is in landscape mode.
+  /// Ex : for tablets, the preview size is given in landscape mode but the device is in portrait mode.
+  inverted() => PreviewSize(width: height, height: width);
 }
 
 class ExifPreferences {
